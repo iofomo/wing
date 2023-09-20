255ca636f5b277fef08ad49ae9183ec7de272c45
chendianbo <dianbo78719>
2023-09-21 01:19:55 +0800

    merge code

--- manager/repo_extend.py
+++ manager/repo_extend.py
@@ -4,8 +4,6 @@
 
 import os, sys
 
-from manager.repo_init import switchBranch
-
 g_this_file = os.path.realpath(sys.argv[0])
 g_this_path = os.path.dirname(g_this_file)
 sys.path.append(os.path.dirname(g_this_path))
@@ -14,7 +12,7 @@ from utils.utils_cmn import CmnUtils
 from utils.utils_logger import LoggerUtils
 from utils.utils_properties import PropertiesUtils
 from manager.repo_env import RepoEnv
-
+from manager.repo_init import switchBranch
 
 # --------------------------------------------------------------------------------------------------------------------------
 class RepoExtend:
