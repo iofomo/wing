92dd39a0aa8f00156191928b1598837322839788
chendianbo <dianbo78719>
2023-09-26 02:04:27 +0800

    update res file path

--- framework/wing_env.py
+++ framework/wing_env.py
@@ -100,7 +100,7 @@ class WingEnv:
 
     @classmethod
     def __do_load_config__(cls):
-        jdata = FileUtils.loadJsonByFile(cls.getWingPath() + os.sep + 'config.json')
+        jdata = FileUtils.loadJsonByFile(cls.getWingPath() + '/res/config.json')
         spaces = jdata['space']
         if spaces is None: return None
         if cls.getSpaceName() not in spaces: return None
